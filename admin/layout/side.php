03808800d905f9ed522a115fdefa8c32fcf64124
rjeth <40770876+rjeth@users.noreply.github.com>
2018-10-23 18:56:10 +0800

    side

--- admin/layout/side.php
+++ admin/layout/side.php
@@ -8,23 +8,12 @@
   <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
       <i class="fas fa-fw fa-folder"></i>
-      <span>Loan</span>
-    </a>
-    <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-      <a class="dropdown-item" href="login.html">Add Loan</a>
-      <div class="dropdown-divider"></div>
-      <a class="dropdown-item" href="../loan">View Management</a>
-    </div>
-  </li>
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      <i class="fas fa-fw fa-folder"></i>
-      <span>Borrower</span>
+      <span>User management</span>
     </a>
     <div class="dropdown-menu" aria-labelledby="pagesDropdown">
       <a class="dropdown-item" href="login.html">Add Borrower</a>
       <div class="dropdown-divider"></div>
-      <a class="dropdown-item" href="../loan">View Management</a>
+      <a class="dropdown-item" href="../user_management">View Management</a>
     </div>
   </li>
 </ul>
