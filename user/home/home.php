bc9bf7e4aee0a73d7628a745d15fe6264d4f0958
rjeth <40770876+rjeth@users.noreply.github.com>
2018-10-24 18:53:02 +0800

    name

--- user/home/home.php
+++ user/home/home.php
@@ -28,6 +28,11 @@
                       </div>
                   </div>
                     <div class="card mb-4 wow fadeIn">
+                      <div class="card-header font-weight-bold">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
                       <!--Card content-->
                         <div class="card-body">
                             <p class="h5 my-4">That's a very long heading </p>
@@ -76,17 +81,17 @@
               <span class="input-group-text">Image</span>
             </div>
             <div class="custom-file">
-              <input type="file" class="custom-file-input" id="fileInput" aria-describedby="fileInput">
+              <input type="file" class="custom-file-input" id="image_file" name="image_file" aria-describedby="fileInput">
               <label class="custom-file-label" for="fileInput">File Label</label>
             </div>
           </div>
           <div class="form-group">
               <label for="postForm">Pet name</label>
-              <input class="form-control" id="postForm" type="text" rows="5"></input>
+              <input class="form-control" id="pet_name" name="pet_name" type="text" rows="5"></input>
           </div>
           <div class="form-group">
             <label for="select">Type</label>
-            <select class="browser-default custom-select" id="select">
+            <select class="browser-default custom-select" id="pet_type" name="pet_type">
               <option value="" disabled="" selected="">Choose type</option>
               <option value="1">Dog</option>
               <option value="2">Cat</option>
@@ -95,11 +100,11 @@
           </div>
           <div class="form-group">
               <label for="postForm">Age</label>
-              <input class="form-control" id="postForm" type="number" rows="5"></input>
+              <input class="form-control" id="pet_age" name="pet_age" type="number" rows="5"></input>
           </div>
           <div class="form-group">
             <label for="select">Dewormed</label>
-            <select class="browser-default custom-select" id="select">
+            <select class="browser-default custom-select" id="pet_dewormed" name="pet_dewormed">
               <option value="" disabled="" selected="">Choose type</option>
               <option value="1">True</option>
               <option value="2">False</option>
@@ -107,17 +112,16 @@
           </div>
           <div class="form-group">
             <label for="select">Vacinated</label>
-            <select class="browser-default custom-select" id="select">
+            <select class="browser-default custom-select" id="pet_vaccinated" name="pet_vaccinated">
               <option value="" disabled="" selected="">Choose type</option>
               <option value="1">True</option>
               <option value="2">False</option>
             </select>
           </div>
-
             <!-- Comment -->
             <div class="form-group">
                 <label for="postForm">Description</label>
-                <textarea class="form-control" id="postForm" rows="5"></textarea>
+                <textarea class="form-control" id="pet_description"  name="pet_description" type="text" rows="5"></textarea>
             </div>
           </div>
       <div class="modal-footer">
