d956db4291248a3bed6660939b34eaa3439ee182
rjeth <40770876+rjeth@users.noreply.github.com>
2018-10-24 19:02:55 +0800

    pullright

--- user/home/home.php
+++ user/home/home.php
@@ -27,6 +27,42 @@
                         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#basicExampleModal">Upload</button>
                       </div>
                   </div>
+                  <div class="card mb-4 wow fadeIn" style="visibility: visible; animation-name: fadeIn;">
+
+                            <div class="card-header font-weight-bold">
+                                <span>About author</span>
+                                <span class="pull-right">
+                                    <a href="">
+                                        <i class="fa fa-facebook mr-2"></i>
+                                    </a>
+                                    <a href="">
+                                        <i class="fa fa-twitter mr-2"></i>
+                                    </a>
+                                    <a href="">
+                                        <i class="fa fa-instagram mr-2"></i>
+                                    </a>
+                                    <a href="">
+                                        <i class="fa fa-linkedin mr-2"></i>
+                                    </a>
+                                </span>
+                            </div>
+                            <!--Card content-->
+                            <div class="card-body">
+                                <div class="media d-block d-md-flex mt-3">
+                                    <img class="d-flex mb-3 mx-auto z-depth-1" src="https://mdbootstrap.com/img/Photos/Avatars/img (30).jpg" alt="Generic placeholder image" style="width: 100px;">
+                                    <div class="media-body text-center text-md-left ml-md-3 ml-0">
+                                        <h5 class="mt-0 font-weight-bold">Caroline Horwitz
+                                        </h5>
+                                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti
+                                        quos dolores et quas molestias excepturi sint occaecati cupiditate non provident,
+                                        similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum
+                                        fuga.
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
                     <div class="card mb-4 wow fadeIn">
                       <!--Card content-->
                         <div class="card-body">
