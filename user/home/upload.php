fe48d5b366d7ffdea0b7b21519d8744fc5c17aaf
auhsojsomar <auhsojsomar@gmail.com>
2018-10-24 23:21:28 +0800

    commit

--- user/home/upload.php
+++ user/home/upload.php
@@ -1,11 +1,13 @@
 <?php
     include '../../core/config.php';
-    $pet_image = $_POST['fileInput'];
+    $pet_image = $_POST['image_file'];
     $pet_name = $_POST['pet_name'];
     $pet_type = $_POST['pet_type'];
     $pet_age = $_POST['pet_age'];
     $pet_dewormed = $_POST['pet_dewormed'];
     $pet_vacinated = $_POST['pet_vacinated'];
     $pet_description = $_POST['pet_description'];
-    
+    $new_name = rand(). '.' ."jpeg";
+	$destination = 'upload/' . $new_name;
+	move_uploaded_file($_FILES['user_image']['tmp_name'], $destination);
 ?>
