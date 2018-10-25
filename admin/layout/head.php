c87ab51e2a75c881cd255e931cddfb4b4f27ce06
rjeth <40770876+rjeth@users.noreply.github.com>
2018-10-26 07:52:32 +0800

    asdasd

--- admin/layout/head.php
+++ admin/layout/head.php
@@ -45,7 +45,7 @@
 
       <!-- Navbar -->
       <ul class="navbar-nav ml-auto ml-md-0">
-        <li class="nav-item dropdown no-arrow mx-1">
+        <!--li class="nav-item dropdown no-arrow mx-1">
           <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <i class="fas fa-bell fa-fw"></i>
             <span class="badge badge-danger">9+</span>
@@ -68,15 +68,15 @@
             <div class="dropdown-divider"></div>
             <a class="dropdown-item" href="#">Something else here</a>
           </div>
-        </li>
+        </li-->
         <li class="nav-item dropdown no-arrow">
           <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <i class="fas fa-user-circle fa-fw"></i>
           </a>
           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-            <a class="dropdown-item" href="../user">Settings</a>
+            <!--<a class="dropdown-item" href="../user">Settings</a>
             <a class="dropdown-item" href="#">Activity Log</a>
-            <div class="dropdown-divider"></div>
+            <div class="dropdown-divider"></div-->
             <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
           </div>
         </li>
