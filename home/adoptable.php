51f7b04991cd1aec5122c1c12f8077c3beae169b
rjeth <40770876+rjeth@users.noreply.github.com>
2018-10-26 08:13:40 +0800

    AS

--- home/adoptable.php
+++ home/adoptable.php
@@ -76,37 +76,55 @@
                           <!--Section: Live preview-->
                           <section>
                               <!--Card Light-->
-                              <div class="card">
-                                  <!--Card image-->
-                                  <div class="view overlay">
-                                      <img src="https://images.pexels.com/photos/1345191/pexels-photo-1345191.jpeg" class="card-img-top" alt="fern">
-                                      <a>
-                                          <div class="mask rgba-white-slight waves-effect waves-light"></div>
-                                      </a>
-                                  </div>
-                                  <!--/.Card image-->
-                                  <!--Card content-->
-                                  <div class="card-body">
-                                      <h4 class="card-title">Pet name </h4>
+                              <div class="row">
 
-                                      <!--Text-->
-                                      <div class="card-text">
-                                        <div >
-                                            <p class="mt-0 mb-1 font-weight-bold text-muted" for="location">Location</p>
-                                            <label id="location">dignissimos ducimus</label>
+                                  <?php
+                                    include_once("../../core/config.php");
 
-                                        </div>
-                                          <div>
-                                            <p class="mt-0 mb-1 font-weight-bold text-muted" for="location">age</p>
-                                            <label id="location">dignissimos ducimus</label>
+                                     $sql = "SELECT * FROM tbl_pets";
+                                     $resultset = mysqli_query($connect, $sql) or die("database error:". mysqli_error($connect));
+                                     while( $record = mysqli_fetch_assoc($resultset) ) {
+                                    ?>
+                                    <div class="col-md-4 mb-4">
+                                        <!--Subheading-->
+                                        <!--Section: Live preview-->
+                                        <section>
 
-                                        </div>
-                                      </div>
-                                      <hr>
-                                      <a data-toggle="modal" data-target="#exampleModalCenter">
-                                        <h5>Read more <i class="fa fa-chevron-right"></i></h5></a>
-                                  </div>
-                                  <!--/.Card content-->
+                                            <div class="card">
+                                                <!--Card image-->
+                                                <div class="view overlay">
+                                                    <img src="../home/upload/<?php echo $record['pet_image']; ?>" class="card-img-top" alt="fern">
+                                                    <a>
+                                                        <div class="mask rgba-white-slight waves-effect waves-light"></div>
+                                                    </a>
+                                                </div>
+                                                <!--/.Card image-->
+                                                <!--Card content-->
+                                                <div class="card-body">
+                                                    <h4 class="card-title"><?php echo $record['pet_name']; ?> </h4>
+
+                                                    <!--Text-->
+                                                    <div class="card-text">
+                                                      <div >
+                                                          <p class="mt-0 mb-1 font-weight-bold text-muted" for="location">Location</p>
+                                                          <label id="location"><?php echo $record['pet_location']; ?></label>
+
+                                                      </div>
+                                                        <div>
+                                                          <p class="mt-0 mb-1 font-weight-bold text-muted" for="location">age</p>
+                                                          <label id="location"><?php echo $record['pet_age']; ?></label>
+                                                      </div>
+                                                    </div>
+                                                    <hr>
+                                                    <?php echo   '<a class="link-text" href="view_pet_info.php?pet_id='.$record['pet_id'].'">'; ?>
+                                                      <h5>Read more <i class="fa fa-chevron-right"></i></h5></a>
+                                                </div>
+
+
+                                        </section>
+                                    </div>
+                                  <?php } ?>
+                                </div>
                               </div>
                               <!--/.Card Light-->
                           </section>
--- user/home/home.php
+++ user/home/home.php
@@ -3,13 +3,6 @@
 <?php include("../../layout/head2.php"); ?>
 <main class="mt-5 pt-5 pb-5">
     <div class="container">
-        <div class="alert alert-success" role="alert">
-            <h4 class="alert-heading">Well done!</h4>
-            <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit
-                longer so that you can see how spacing within an alert works with this kind of content.</p>
-            <hr>
-            <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
-        </div>
         <!--Section: Post-->
         <section class="mt-4">
             <div class="row">
